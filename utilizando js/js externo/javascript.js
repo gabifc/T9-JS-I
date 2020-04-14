f18f40c65ce8a1fc68d3c6607c1a12d4102dafe0
Bruna Vieira <bruna.vieira@supplylabs.com.br>
2020-04-13 23:22:43 -0300

    aula 1

---
+++ utilizando js/js externo/javascript.js
@@ -0,0 +1,25 @@
+
+const nome = "Bruna";
+
+const num = 5;
+const ativo = true;
+
+console.log('nome ', typeof(nome));
+console.log('num ', typeof(num));
+console.log('ativo', typeof(ativo));
+
+
+//alert('Olá Reprograma! Sou um js externo');
+
+ let capturarP = document.querySelector('p');
+ capturarP.addEventListener('click', clicarNoBotao);
+ 
+ function clicarNoBotao(){
+     console.log('oi meninas')
+     console.info('oi meninas')
+     console.error('oi meninas')
+     console.warn('oi meninas')
+ }
+
+
+//teste.addEventListener('click', cli)
