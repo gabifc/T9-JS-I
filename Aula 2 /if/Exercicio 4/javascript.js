eded7493de87dd41ab445e154a5cf03f1536abb2
Bruna Vieira <bruna.t@dasa.com.br>
2020-04-14 14:27:46 -0300

    exercicio 4 comentado

--- Aula 2 /if/Exercicio 4/javascript.js
+++ Aula 2 /if/Exercicio 4/javascript.js
@@ -3,9 +3,9 @@ let y = parseInt(prompt('valor 2'))
 let z = 0;
 console.log(`valores iniciais ${x}, ${y}, ${z}`)
 
-z = x;
-x = y;
-y = z;
+z = x; //salvando valor inicial do x    - z = 10
+x = y; //igualei x e y    - x=30 y=30
+y = z; //pegar o valor inicial do x que foi salvo no z y=10 e x=30
 z = 0;
 
 console.log(`valores finais ${x}, ${y}, ${z}`)
