865ddbe888251c281975e27e1763f2439a2d59c5
Bruna Vieira <bruna.t@dasa.com.br>
2020-04-14 16:25:56 -0300

    exercicio if ternario

---
+++ Aula 2 /if ternario/Exercicio 3/javascript.js
@@ -0,0 +1,8 @@
+let isIphone = false;
+let os = "";
+
+(isIphone === true) ? os = "iOS" : os = "Android";
+
+console.log(os);
+
+//os = (isIphone) ? "iOS" : "Android";
---
+++ Aula 2 /if ternario/Exercicio 4/javascript.js
@@ -0,0 +1,6 @@
+let salario = prompt('digite o salario');
+//let cargo = "";
+(salario < 5000) ? cargo="junior": cargo="pleno";
+
+//let cargo = (salario < 5000) ? "junior": "pleno";
+console.log(cargo)
